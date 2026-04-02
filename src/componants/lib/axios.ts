280c527452e8c4a77574f1d95f263681694c4777
Fitrilv3526c <IwillBeBillionareOneDay@gmail.com>
2026-04-02 13:08:53 +0500

    fix axios baseURL to include /api

--- src/componants/lib/axios.ts
+++ src/componants/lib/axios.ts
@@ -1,9 +1,9 @@
-
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL + "/api" || "http://localhost:4000/api",
   withCredentials: true, // 👈 enables cookies to be sent & received
-})
+});
 
-export default api
+export default api;
